7d4e258ed2893f0d6dd0fe08a6cdb5f9fe953473
Tom Courcy <tom.courcy@gmail.com>
2025-02-07 14:42:27 +0100

    Initial commit: Setup React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
